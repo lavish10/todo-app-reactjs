a04d8f75f3a47249654db33cc3053b41951c521f
lavish.yadav <lavishyadav@live.com>
2019-08-30 00:10:08 +0530

    [Lavish] Add. TaskView to component

--- src/components/TaskView.js
+++ src/components/TaskView.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import '../App.css';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-export default function TaskView(props) {
-    return (
-        <li id={`task_${props.id}`}>{props.task}</li>
-    );
+export default class TaskView extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <li id={`task_${this.props.id}`}>{this.props.task}</li>
+        );
+    }
 }
+
+TaskView.propType = {
+    id: PropTypes.string.isRequired,
+    task: PropTypes.string.isRequired
+};
